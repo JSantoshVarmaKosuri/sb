a5b1e6f1b48edef6b1d77504aca17d3b9a76ed86
sandy1013 <santosh.kosuri@gmail.com>
2017-11-27 18:28:20 +0530

    heder base complete

--- src/app/SBSharedModule/sb.shared.module.ts
+++ src/app/SBSharedModule/sb.shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 
 import { SbHeaderComponent } from "./Components/HeaderComponent/sb.header.component";
 
 @NgModule({
     declarations: [SbHeaderComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     exports: [
         CommonModule,
         SbHeaderComponent
